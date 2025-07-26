28961b883fd0e90a89a9e628c5f2f7629736f2a7
Manul <test@test.com>
2025-07-26 13:41:24 +0300

    create log cleaner

---
+++ log_cleaner.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+LOG_DIR="/var/log"
+
+echo "Starting log cleaner in $LOG_DIR"
+
+if [ ! -d "$LOG_DIR" ]; then
+	echo "$LOG_DIR does not exist" >&2
+	exit 1
+fi
+
+find "$LOG_DIR" -name "*.log" -type f -mtime +7 -exec rm -f {} \;
+
+echo "Logs older than 7 days in $LOG_DIR is cleaning!"
