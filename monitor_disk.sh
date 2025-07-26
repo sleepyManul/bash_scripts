829f82c625e9be28338b522b655b6207886fad82
Manul <test@test.com>
2025-07-26 19:12:51 +0300

    changed alert_cd

--- monitor_disk.sh
+++ monitor_disk.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 source ~/.config/telegram_bot.conf
-WARNING_LIMIT=5
-LAST_ALERT_TIME=0
+WARNING_LIMIT=90
 ALERT_CD=300
 
 tg_alert_func() {
@@ -18,16 +17,12 @@ check_disk_func() {
 	local disk="$1"
 	local use=$(df -h "$disk" | awk 'NR==2 {print $5}' | tr -d '%')
 	if [ "$use" -ge "$WARNING_LIMIT" ]; then
-		ALERT_TIME=$(date +%s)
-		if (( ALERT_TIME - LAST_ALERT_TIME >= ALERT_CD )); then
-			tg_alert_func "$disk" "$use"
-			LAST_ALERT_TIME="$ALERT_TIME"
-		fi
+		tg_alert_func "$disk" "$use"
 	fi
 }
 
 while true; do
         check_disk_func "/"
         check_disk_func "/home"
-        sleep 10
+        sleep "$ALERT_CD"
 done
