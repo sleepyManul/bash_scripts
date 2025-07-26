c362a05734af9d0dc53201bf18bb7b7ca02b4b4c
Manul <test@test.com>
2025-07-26 18:43:53 +0300

    add func tg alert

--- monitor_disk.sh
+++ monitor_disk.sh
@@ -3,13 +3,20 @@
 source ~/.config/telegram_bot.conf
 WARNING_LIMIT=5
 
+tg_alert_func() {
+	local disk="$1"
+	local use="$2"
+	local message="WARNING! Section < $disk > is filled to $use%!"
+		curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
+                -d "chat_id=$TG_CHAT_ID" \
+                -d "text=$message" >/dev/null 2>&1
+}
+
 check_disk_func() {
-	local disk=$1
+	local disk="$1"
 	local use=$(df -h "$disk" | awk 'NR==2 {print $5}' | tr -d '%')
 	if [ "$use" -ge "$WARNING_LIMIT" ]; then
-		curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
-		-d "chat_id=$TG_CHAT_ID" \
-		-d "text=WARNING! $disk filled to $use%!" >/dev/null 2>&1
+		tg_alert_func "$disk" "$use"
 	fi
 }
 
